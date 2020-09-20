7f59428c587a948eef78da85edf764bcd2303605
Rafi <rafael.cohngruenwald@gmail.com>
2020-09-19 19:46:48 -0700

    made and pickled model class

--- src/python/helpers.py
+++ src/python/helpers.py
@@ -1,11 +1,73 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 from imblearn.over_sampling import SMOTE, RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import KFold
 from sklearn.metrics import precision_score, recall_score, f1_score
+
+def dummify(X):
+    dummy_cols = ['total_nights', 'total_of_special_requests', 'market_segment', 'party_size']
+    X = pd.get_dummies(X, columns=dummy_cols, drop_first=True)
+    return X.to_numpy()
+
+def clean_data(data):
+    feats = ['hotel','market_segment', 'total_of_special_requests', 
+         'total_nights', 'room_difference', 'party_size', 'booking_changes']
+    X = data[feats].copy()
+    
+    
+    dummifiable_df = pd.DataFrame({
+            'hotel' : [0, 1, 0, 1, 0, 0, 1, 1],
+            'market_segment' : ['Offline TA/TO', 'Online TA', 
+                                'Groups', 'Direct', 'Corporate',
+                                'Groups', 'Direct', 'Corporate'],
+            'total_of_special_requests' : [0, 1, 2, 0, 1, 2, 0, 1] ,
+            'total_nights' : [1, 2, 3, 4, 5, 6, 7, 8],
+            'room_difference' : [0, 1, 0, 1, 0, 1, 0, 1],
+            'party_size' : [1, 2, 3, 3, 2, 1, 1, 2],
+            'booking_changes' : [0, 0, 1, 1, 0, 0, 1, 1]
+            })
+    
+    X = X.append(dummifiable_df, ignore_index=True)
+    
+    
+    X.replace(['Resort Hotel', 'City Hotel'], [0, 1], inplace=True)
+    X.loc[np.argwhere((X['total_nights'] >= 8).values).flatten(), 'total_nights'] = 8
+    X.loc[np.argwhere((X['total_of_special_requests'] >= 2).values).flatten(), 'total_of_special_requests'] = 2
+    X.loc[np.argwhere(X['market_segment'].isin(['Aviation', 'Complementary']).values).flatten(), 'market_segment'] = 'Corporate'
+    X.loc[np.argwhere((X['party_size'] >= 3).values).flatten(), 'party_size'] = 3
+    X.loc[np.argwhere((X['booking_changes'] >= 1).values).flatten(), 'booking_changes'] = 1
+    X = dummify(X)
+        
+    y = data[['is_canceled']].to_numpy()
+    
+    return X[0 : data.shape[0], :], y
+
+def get_day_counts(data):
+    start_day = (data['full_date'] - np.min(data['full_date'])).dt.days
+    days = len(data['full_date'].unique())
+    resort_bookings_by_day = np.zeros(days)
+    city_bookings_by_day = np.zeros(days)
+    resort_cancellations_by_day = np.zeros(days)
+    city_cancellations_by_day = np.zeros(days)
+
+    for day, nights, hotel_type, canceled in zip(start_day, data['total_nights'], data['hotel'], data['is_canceled']):
+        if canceled == 1:
+            if hotel_type == 1:
+                city_cancellations_by_day[day:(day + nights + 1)] += 1
+            else:
+                resort_cancellations_by_day[day:(day + nights + 1)] += 1
+        else:  
+            if hotel_type == 1:
+                city_bookings_by_day[day:(day + nights + 1)] += 1
+            else:
+                resort_bookings_by_day[day:(day + nights + 1)] += 1
+                
+    return resort_bookings_by_day, resort_cancellations_by_day, city_bookings_by_day, city_cancellations_by_day
+
 
 def balance_train_data(X, y, method=None):
     '''
@@ -85,3 +147,79 @@ def plot_cross_val(models, X, y, ax, sampling_method, names, n_splits=5):
         #ax.plot(x, recalls[i], c=colormap[i], 
         #        linewidth=1, linestyle='--',
         #        label='%s Recall' % names[i])
+        
+        
+def moving_avg(values, window=3):
+    bef = np.floor(window/2)
+    aft = np.ceil(window/2)
+    avg = []
+    for i in range(len(values)):
+        before = 0 if i < bef else i - bef
+        after = len(values) if i + aft > len(values) else i + aft
+        avg.append(np.mean(values[int(before) : int(after)]))
+    return avg
+
+def timedelta(series):
+    deltas = []
+    for s in series:
+        deltas.append(datetime.timedelta(days=s))
+        
+    return pd.Series(deltas, index=series.index)
+
+
+def plot_trend_data(ax, name, series):
+    ax.plot(series.index.date, series, linewidth=2, label='Raw Data')
+
+def make_design_matrix(arr):
+    """Construct a design matrix from a numpy array, converting to a 2-d array
+    and including an intercept term."""
+    return sm.add_constant(arr.reshape(-1, 1), prepend=False)
+
+def fit_linear_trend(series):
+    """Fit a linear trend to a time series.  Return the fit trend as a numpy array."""
+    X = make_design_matrix(np.arange(len(series)) + 1)
+    linear_trend_ols = sm.OLS(series.values, X).fit()
+    linear_trend = linear_trend_ols.predict(X)
+    return linear_trend
+
+def plot_linear_trend(ax, name, series):
+    linear_trend = fit_linear_trend(series)
+    plot_trend_data(ax, name, series)
+    ax.plot(series.index.date, linear_trend, linewidth=2, label='Trend')
+    
+def create_monthly_dummies(series):
+    month = series.index.week
+    # Only take 11 of the 12 dummies to avoid strict colinearity.
+    return pd.get_dummies(month, drop_first=True)
+
+def fit_seasonal_trend(series):
+    dummies = create_monthly_dummies(series)
+    X = sm.add_constant(dummies.values, prepend=False)
+    seasonal_model = sm.OLS(series.values, X).fit()
+    return seasonal_model.predict(X)
+
+def plot_seasonal_trend(ax, name, series):
+    seasons_average_trend = fit_seasonal_trend(series)
+    plot_trend_data(ax, name, series, )
+    ax.plot(series.index.date, seasons_average_trend, '-', label='Seasonal Trend', linewidth=2)
+    
+def plot_decomposition(ax, name, series):
+    plot_trend_data(ax[0], name, series)
+    ax[0].set_title('Raw Series - %s' % (name), fontsize=20)
+    
+    linear_trend = fit_linear_trend(series)
+    plot_trend_data(ax[1], name, pd.Series(linear_trend, series.index))
+    ax[1].set_title('Trend Component - %s' % (name), fontsize=20)
+    
+    if name == 'Resort':
+        ax[1].set_ylim(150, 350)
+    else:
+        ax[1].set_ylim(200, 600)
+    
+    seasonal_trend = fit_seasonal_trend(series - linear_trend)
+    plot_trend_data(ax[2], name, pd.Series(seasonal_trend, series.index))
+    ax[2].set_title('Seasonal Component - %s' % (name), fontsize=20)
+    
+    plot_trend_data(ax[3], name, series - seasonal_trend - linear_trend)
+    ax[3].set_title('Residual Component - %s' % (name), fontsize=20)
+    
---
+++ src/python/model.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pickle
+import statsmodels.api as sm
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+
+class Model:
+    def __init__(self):
+        self.high_demand_thresh = 0.81
+        self.low_demand_thresh = 0.21
+        self.model = RandomForestClassifier(criterion='entropy', max_depth=10, 
+                                      max_features='sqrt', n_estimators=75)
+        self.resort_rooms = 250
+        self.city_rooms = 350
+        
+    def make_design_matrix(self, arr):
+            """Construct a design matrix from a numpy array, converting to a 2-d array
+            and including an intercept term."""
+            return sm.add_constant(arr.reshape(-1, 1), prepend=False)
+
+    def fit_linear_trend(self, series):
+        """Fit a linear trend to a time series.  Return the fit trend as a numpy array."""
+        X = self.make_design_matrix(np.arange(len(series)) + 1)
+        linear_trend_ols = sm.OLS(series.values, X).fit()
+        linear_trend = linear_trend_ols.predict(X)
+        return linear_trend
+    
+    def fit_classifier(self, X, y):
+        self.X = X
+        self.y = y.flatten()
+        self.model.fit(self.X, self.y)
+        
+    def fit_demand_forecaster(self, resort_dates, city_dates):
+        df = pd.DataFrame()
+        df['ds'] = resort_dates.index
+        df['y'] = resort_dates.values
+        df = df[15:]
+
+        df['month'] = df['ds'].dt.month
+        df['day_of_month'] = df['ds'].dt.day
+        df['year'] = df['ds'].dt.year
+        
+        self.resort_dates = df.copy()
+        l = self.fit_linear_trend(resort_dates)
+        self.resort_slope = 1 + (l[1] - l[0] / 1)
+                      
+        df = pd.DataFrame()
+        df['ds'] = city_dates.index
+        df['y'] = city_dates.values
+        df = df[15:]
+
+        df['month'] = df['ds'].dt.month
+        df['day_of_month'] = df['ds'].dt.day
+        df['year'] = df['ds'].dt.year
+        
+        self.city_dates = df.copy()
+        l = self.fit_linear_trend(city_dates)
+        self.city_slope = 1 + (l[1] - l[0] / 1)
+       
+    def predict_demand(self, hotel_type, date):
+        if type(date) == str:
+            [yr, m, d] = date.split('-')
+        else:
+            m = date.month
+            d = date.day
+
+        # hotel_type 1: city
+        if hotel_type == 1:
+            return (self.city_dates[(self.city_dates['month'] == int(m)) & 
+                           (self.city_dates['day_of_month'] == int(d))]['y'].mean() * self.city_slope)
+        else:
+            return (self.resort_dates[(self.resort_dates['month'] == int(m)) & 
+                           (self.resort_dates['day_of_month'] == int(d))]['y'].mean() * self.resort_slope)
+        
+    
+    def predict_proba(self, X, dates):
+        y_probas = self.model.predict_proba(X)[:, 1]
+        
+        demands = []
+        for hotel_type, date in zip(X[:, 0], dates):
+            forecasted_demand = self.predict_demand(hotel_type, date)
+            demands.append(int(forecasted_demand))
+           
+        return (y_probas, np.array(demands))
+        
+        
